2d5d2a233d2e63d4a54f2209fa43eedb1e8242d2
chme <christian.meffert@googlemail.com>
2019-01-03 11:00:55 +0100

    Connect to forked-daapd websocket

--- server/forked_daapd.py
+++ server/forked_daapd.py
@@ -5,19 +5,28 @@ import aiohttp
 
 class ForkedDaapd:
 
-    def __init__(self, loop, conf):
+    def __init__(self, loop):
         self.loop = loop
-        self.client = self.loop.run_until_complete(aiohttp.ClientSession())
+        self.running = True
 
     async def notify_loop(self):
-        async with session.ws_connect('http://example.org/ws') as ws:
-            await ws.send_str('data')
-            async for msg in ws:
-                if msg.type == aiohttp.WSMsgType.TEXT:
-                    if msg.data == 'close':
-                        await ws.close()
-                        break
-                    else:
-                        await ws.send_str(msg.data + '/answer')
-                elif msg.type == aiohttp.WSMsgType.ERROR:
-                    break
+        async with aiohttp.ClientSession() as client:
+            while self.running:
+                try:
+                    async with client.ws_connect('http://localhost:3688', protocols=('notify',)) as ws:
+                        print('Connection established')
+                        await ws.send_json({ 'notify': ['player', 'outputs', 'volume'] })
+                        async for msg in ws:
+                            print(msg.data)
+                            if msg.type == aiohttp.WSMsgType.TEXT:
+                                if msg.data == 'close':
+                                    await ws.close()
+                                    break
+                            elif msg.type == aiohttp.WSMsgType.ERROR:
+                                break
+                    print('Connection closed')
+                except:
+                    print('Connection failed')
+                    await asyncio.sleep(1)
+
+            print('Session closed')
